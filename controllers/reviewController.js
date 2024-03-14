8129cdde711b1210a13194e878f154cf30e9c3d6
benhere <benjamin.oldfriend@gmail.com>
2024-03-14 17:30:41 +0530

    e-commerce-apis_1.14_review_apis_started

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -1,10 +1,39 @@
 
+const Review = require('../models/reviewModel');
+const Product = require('../models/productModel');
+const { StatusCodes } = require('http-status-codes')
+const CustomError = require('../errors');
+const { checkPermissions } = require('../utils/index');
 
 
 // review APIs
 
 const createReview = async(req,res) => {
-    res.send('review created...')
+    const { product: productId } = req.body;
+    
+    const isValidProduct = await Product.findOne({ _id: productId });
+
+    // before giving review check whether the product exist or not
+    if(!isValidProduct){
+        throw new CustomError.NotFoundError(`No product with id : ${productId}`);
+    }
+
+    // check if user already given review to the product
+    const alreadySubmitted = await Review.findOne({
+        product: productId,
+        user: req.user.userId
+    });
+
+    if(alreadySubmitted){
+        throw new CustomError.BadRequestError(`Already submitted review for this product`);
+    }
+
+    // attach user property (set it to equal to req.user.userId) onto req.body
+    req.body.user = req.user.userId;
+
+    // create review
+    const review = await Review.create(req.body);
+    res.status(StatusCodes.CREATED).json({ review });
 }
 
 const getAllReviews = async(req,res) => {
