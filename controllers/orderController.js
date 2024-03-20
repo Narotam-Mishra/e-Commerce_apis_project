bd551b1824451fbd0f32c84137c61e5144198b20
benhere <benjamin.oldfriend@gmail.com>
2024-03-20 13:47:37 +0530

    e-commerce-apis_1.20_order_api_completed

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -5,6 +5,8 @@ const { StatusCodes } = require('http-status-codes')
 const CustomError = require('../errors');
 const { checkPermissions, getRandomText } = require('../utils/index');
 
+// Order APIs
+
 const fakeStripeAPI = async (amount, currency) => {
     const client_secret = getRandomText();
     return {client_secret, amount};
@@ -74,22 +76,51 @@ const createOrder = async (req,res) => {
     res.status(StatusCodes.CREATED).json({ order, clientSecret:order.clientSecret});
 }
 
-const getAllOrders = (req,res) => {
-    res.send('get all orders...');
+const getAllOrders = async (req,res) => {
+    const orders = await Order.find({});
+    res.status(StatusCodes.OK).json({ orders, count:orders.length })
 }
 
-const getSingleOrder = (req,res) => {
-    res.send('get single order...');
+const getSingleOrder = async (req,res) => {
+    const { id:orderId } = req.params;
+    const order = await Order.findOne({ _id:orderId });
+
+    // if order doesn't exist then throw error
+    if(!order){
+        throw new CustomError.NotFoundError(`No order found with id: ${orderId}`);
+    }
+
+    // check permission
+    checkPermissions(req.user, order.user)
+    res.status(StatusCodes.OK).json({ order });
 }
 
-const getCurrentUserOrders = (req,res) => {
-    res.send('get current user orders...');
+const getCurrentUserOrders = async (req,res) => {
+    const orders = await Order.find({ user:req.user.userId });
+    res.status(StatusCodes.OK).json({ orders, count:orders.length });
 }
 
-const updateOrder = (req,res) => {
-    res.send('order updated...');
-}
+const updateOrder = async (req,res) => {
+    const { id:orderId } = req.params;
+    const { paymentIntentId } = req.body;
 
+    const order = await Order.findOne({ _id:orderId });
+
+    // if order doesn't exist then throw error
+    if(!order){
+        throw new CustomError.NotFoundError(`No order found with id: ${orderId}`);
+    }
+
+    // check permission
+    checkPermissions(req.user, order.user)
+    
+    // update 'paymentIntentId' and payment's status (to paid)
+    order.paymentIntentId = paymentIntentId;
+    order.status = 'paid';
+    await order.save();
+
+    res.status(StatusCodes.ACCEPTED).json({ order });
+}
 
 module.exports = {
     getAllOrders,
